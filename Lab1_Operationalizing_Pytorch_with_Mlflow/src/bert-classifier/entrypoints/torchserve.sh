9637d9c8e32df429d43db274bceffa837faadce7
gitadmin <gitadmin@mykube.awesome>
2021-01-27 21:28:28 +0000

    Add a flag to explicitly state the torchserve model_store location

--- Lab1_Operationalizing_Pytorch_with_Mlflow/src/bert-classifier/entrypoints/torchserve.sh
+++ Lab1_Operationalizing_Pytorch_with_Mlflow/src/bert-classifier/entrypoints/torchserve.sh
@@ -7,4 +7,5 @@ mlflow deployments create \
         -m models:${MODEL_NAME} \
         --name news_classification \
         -C "MODEL_FILE=news_classifier.py" \
-        -C "HANDLER=news_classifier_handler.py"
+        -C "HANDLER=news_classifier_handler.py"\
+        -C "EXPORT_PATH=/opt/mlflow/model_store"
