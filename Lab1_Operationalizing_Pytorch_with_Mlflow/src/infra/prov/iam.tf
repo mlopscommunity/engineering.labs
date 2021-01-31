46608a9eb04978622f7ce65a16355d4614b48aa5
Michel Vasconcelos <michel.vasconcelos@gmail.com>
2021-01-31 03:18:29 -0300

    Adding roles to service account

--- Lab1_Operationalizing_Pytorch_with_Mlflow/src/infra/prov/iam.tf
+++ Lab1_Operationalizing_Pytorch_with_Mlflow/src/infra/prov/iam.tf
@@ -16,3 +16,19 @@ resource "google_project_iam_member" "iam_storage_writer" {
   role   = "roles/storage.objectAdmin"
   member = "serviceAccount:${google_service_account.mlflow_data_account.email}"
 }
+
+resource "google_project_iam_member" "iam_storage_admin" {
+  role   = "roles/storage.admin"
+  member = "serviceAccount:${google_service_account.mlflow_data_account.email}"
+}
+
+resource "google_project_iam_member" "iam_run_admin" {
+  role   = "roles/run.admin"
+  member = "serviceAccount:${google_service_account.mlflow_data_account.email}"
+}
+
+# In order to deploy in Cloud Run
+resource "google_project_iam_member" "iam_act_as" {
+  role   = "roles/iam.serviceAccountUser"
+  member = "serviceAccount:${google_service_account.mlflow_data_account.email}"
+}
